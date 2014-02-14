f2b16966d66d0907b4a8ee360392bdaa4387b251
David Nunez <david@davidnunez.com>
2014-02-13 21:43:24 -0500

    updates to motorola xoom deploy

--- devices/motorola-xoom/deploy.sh
+++ devices/motorola-xoom/deploy.sh
@@ -6,7 +6,9 @@ LABEL="$DEPLOYMENT_CODE-$1"
 TUNNEL_PORT="$TUNNEL_PORT_PREFIX$1"
 SDCARD_INT="/data/media/0"
 SDCARD_INT_ESCAPED="\/data\/media\/0"
-SDCARD_EXT=""
+SDCARD_EXT="/sdcard"
+SDCARD_EXT_ESCAPED="\/sdcard"
+
 # --------------------------------------
 
 if [[ $# -lt 1 ]] ; then
@@ -24,19 +26,20 @@ else
   echo "No WIFI setup supplied."
 fi     # $String is null.
 
+cat ./devices/$DEVICE/openrecoveryscript.template | sed "s/CYANOGENMOD/$CYANOGENMOD/g" |sed "s/SDCARD_EXT/$SDCARD_EXT_ESCAPED/g" | sed "s/SDCARD_INT/$SDCARD_INT_ESCAPED/g" | sed "s/LABEL/$LABEL/g" | sed "s/VERSION/$VERSION/g" | sed "s/TUNNEL_PORT/$TUNNEL_PORT/g" | sed "s/WIFI_SSID/$WIFI_SSID/g" | sed "s/WIFI_PASSWORD/$WIFI_PASSWORD/g"  > ./devices/$DEVICE/openrecoveryscript
 
 read -p "Press [Enter] key to start Flash..."
 
 adb reboot bootloader
-sleep 30
+sleep 15
 fastboot flash recovery ./deployment/$DEPLOYMENT_CODE/$TWRP
 fastboot reboot
-sleep 45
+sleep 60
+adb reboot recovery
+read -p "Press [Enter] key when in TRWP recovery (menu grid appears)..."
+adb push ./devices/$DEVICE/openrecoveryscript /cache/recovery
 adb reboot recovery
 
-# read -p "Press [Enter] key when in TRWP recovery ..."
-
-# cat ./devices/$DEVICE/openrecoveryscript.template | sed "s/SDCARD_INT/$SDCARD_INT_ESCAPED/g" | sed "s/LABEL/$LABEL/g" | sed "s/VERSION/$VERSION/g" | sed "s/TUNNEL_PORT/$TUNNEL_PORT/g" > ./devices/$DEVICE/openrecoveryscript
 
 # sleep 5
 # adb shell 'mkdir -p "$SDCARD_INT/sdcard-ext"'
